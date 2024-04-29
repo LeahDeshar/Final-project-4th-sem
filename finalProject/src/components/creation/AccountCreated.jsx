1f9ab61640c966e6fde48a9a12d0e488002ad6f1
Leah Deshar <leah.desar@gmail.com>
2024-04-29 15:32:44 +0545

    refactor

--- finalProject/src/components/creation/AccountCreated.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AccountCreated() {
-  return (
-    <div className='account'>AccountCreated</div>
-  )
-}
-
-export default AccountCreated
--- finalProject/src/components/factory/customerFactory/Createpost.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../factory.css'
-function Createpost() {
-  return (
-    <div className='factory__container'>Createpost</div>
-  )
-}
-
-export default Createpost
